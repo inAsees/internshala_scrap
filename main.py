ab71543b4cd7af2753eeaadab36cd5198a219b8e
inAsees <gurasees.singh121@gmail.com>
2022-04-28 13:20:19 +0530

    Minor change(object variable cli_handler removed )

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from test_cli_handler import CliHandler
+from tests.cli_handler import CliHandler
 
 
 def main():
-    cli_handler = CliHandler().run()
+    CliHandler().run()
 
 
 if __name__ == "__main__":
