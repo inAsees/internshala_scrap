a389bcbe66af944b6cd718cf27ac0bb988d752dc
inAsees <gurasees.singh121@gmail.com>
2022-04-28 11:44:53 +0530

    New method get_available_keywords created outside the class ScrapInternshala.

--- test_cli_handler.py
+++ test_cli_handler.py
@@ -1,4 +1,4 @@
-from test_scrapper import ScrapInternshala
+from test_scrapper import ScrapInternshala, get_available_keywords
 
 
 class CliHandler:
@@ -9,15 +9,7 @@ class CliHandler:
     def run():
         print("<<<<Welcome Message>>>>")  # To Do.
         print("Pick one job title from the available list using index number adjacent to it.")
-        available_keywords = ["Web Development", "Mobile App Development", "Full Stack Development",
-                              "Flutter Development", "WordPress Development", "Backend Development",
-                              "Android App Development", "Software Development", "Python Development",
-                              "Software Testing", "PHP Development", "Java Development", "React Native Development",
-                              "Front End Development", "Node.js Development", "ReactJS Development",
-                              ".NET Development", "Product Management", "Blockchain Development",
-                              "Django Development", "iOS App Development", "Machine Learning",
-                              "Artificial Intelligence (AI)", "MERN Stack Development", "Quality Assurance",
-                              "Web Design", "Cyber Security", ]
+        available_keywords = get_available_keywords()
         print("Index  Job title")
         for i in range(len(available_keywords)):
             print("{0}--->  {1}".format(i, available_keywords[i]))
