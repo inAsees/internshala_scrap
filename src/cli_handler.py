e9832979345b2690a0eaf9708b4d22b61ea3923d
inAsees <gurasees.singh121@gmail.com>
2022-04-30 21:23:26 +0530

    Major refactorings done. 1. Some imports,such as, sys,Path,Optional. 2. class AttemptsHandling name changed to AttemptsHandler 2. Utils imported from src.utils. 3. 5 passed as an argument of int type in the constructor of AttemptsHandler. 4. enumerate() function used in for loop to print all the available keywords. 5. Following new methods created as follows,     a. _prompt_user_for_overriding     b. _get_idx_from_user_input     c. _is_user_idx_in_range 6. Inside while loop some refactoring is done the make the code short and efficient.

--- src/cli_handler.py
+++ src/cli_handler.py
@@ -1,31 +1,33 @@
-from src.scrapper import ScrapInternshala, AttemptsHandling, get_available_keywords, is_file_path_exists, \
-    is_file_parent_exists
+import sys
+from pathlib import Path
+from typing import Optional
+from src.scrapper import ScrapInternshala, AttemptsHandler
+from src.utils import Utils
 
 
 class CliHandler:
-    def __init__(self):
-        self._attempt_handling = AttemptsHandling()
+    def _init_(self):
+        self._attempt_handler = AttemptsHandler(5)
 
     def run(self):
         print("<<<<Welcome Message>>>>\n"
               "This program will help you to scrap data from www.internshala.com website and write it into CSV file in "
               "your local system.\n")
         print("Pick one job title from the available list using index number adjacent to it.\n")
-        available_keywords = get_available_keywords()
+        available_keywords = Utils.get_available_keywords()
         print("INDEX  JOB TITLE")
-        for i in range(len(available_keywords)):
-            print("{0}--->  {1}".format(i, available_keywords[i]))
+        for i, keyword in enumerate(available_keywords):
+            print(i, "--->", keyword)
 
         user_input = input("\nEnter the index number:")
-        flag = False
-        for idx in range(len(available_keywords)):
-            if user_input == str(idx):
-                flag = True
-                break
-        if not flag:
-            print("Invalid input.\n"
-                  "Run the program again.")
-            quit()
+        user_input_int = self._get_idx_from_user_input(user_input)
+        if user_input_int is None:
+            print("Invalid input.")
+            sys.exit()
+        if not self._is_user_idx_in_range(user_input_int, len(available_keywords)):
+            print("Invalid input")
+            sys.exit()
+
         keyword_search = available_keywords[int(user_input)]
         scrapper = ScrapInternshala(keyword_search)
         scrapper.scrap_all_pages()
@@ -34,38 +36,50 @@ class CliHandler:
               "If you have already saved data and trying to save new data it is highly recommended to provide different"
               " file name.\n")
 
-        while self._attempt_handling.is_cur_attempt_less_than_max_attempt():
-            file_path = input("Enter the path:  ")
-            if is_file_path_exists(file_path):
-                print("This file already exists!!!.")
-                user_choice = input("Do you want to overwrite this file (Y/N): ").lower()
-                if user_choice == "y":
-                    print("Ready to overwrite the file.")
-                    scrapper.dump(file_path)
-                    quit()
-                elif user_choice == "n":
-                    print("Provide different file name with complete path again.")
-                    self._attempt_handling.increment_cur_attempt()
-                    continue
-                else:
-                    print("Invalid input.")
-                    self._attempt_handling.increment_cur_attempt()
-                    continue
-            elif is_file_path_exists(file_path) is None:
+        while self._attempt_handler.is_attempts_left():
+            file_path = Path(input("Enter the path:  "))
+            if not Utils.is_file_extension_supported(file_path):
                 print("Invalid file suffix!!\n"
                       "File suffix should be '.csv'")
-                self._attempt_handling.increment_cur_attempt()
+                self._attempt_handler.increment_cur_attempt()
                 continue
-            if is_file_parent_exists(file_path):
-                scrapper.dump(file_path)
-                quit()
-            elif is_file_parent_exists(file_path) is None:
-                print("Invalid file suffix!!\n"
-                      "File suffix should be '.csv'")
-                self._attempt_handling.increment_cur_attempt()
-                continue
-            print("Invalid path!!")
-            self._attempt_handling.increment_cur_attempt()
-        print("Too many wrong attempts.\n"
+            else:
+                if Utils.is_file_path_exists(file_path):
+                    is_override = self._prompt_user_for_overriding()
+                    if not is_override:
+                        print("Provide different file name with complete path again.")
+                        self._attempt_handler.increment_cur_attempt()
+                        continue
+                    elif is_override is None:
+                        print("Invalid input.")
+                        continue
+                if not Utils.is_parent_path_exists(file_path):
+                    print("Invalid path!!")
+                    self._attempt_handler.increment_cur_attempt()
+                    continue
+                scrapper.dump(str(file_path))
+        print("Too many wrong attempts\n"
               "Program stopped.")
-        quit()
+        sys.exit()
+
+    @staticmethod
+    def _prompt_user_for_overriding() -> Optional[bool]:
+        user_input = input("(y/n):  ").lower()
+        if user_input == "y":
+            return True
+        elif user_input == "n":
+            return False
+        return None
+
+    @staticmethod
+    def _get_idx_from_user_input(user_input: str) -> Optional[int]:
+        try:
+            return int(user_input)
+        except ValueError:
+            return None
+
+    @staticmethod
+    def _is_user_idx_in_range(user_input_int: int, max_len: int) -> bool:
+        if user_input_int is None or user_input_int < 0 or user_input_int > max_len:
+            return False
+        return True
