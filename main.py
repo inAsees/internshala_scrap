2850a0daaddec281a9a5c143a1b3cbb41700d015
inAsees <gurasees.singh121@gmail.com>
2022-05-03 19:44:17 +0530

    Parameters added inside logging.basicConfig

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from src.cli_handler import CliHandler
 import logging
 from pathlib import Path
 
-logging.basicConfig(filename=Path('scrap_logger.log'), encoding='utf-8',
-                    level=logging.INFO, filemode="w")
+logging.basicConfig(filename=Path('scrap_logger.log'), encoding='utf-8', level=logging.INFO, filemode="w",
+                    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def main():
