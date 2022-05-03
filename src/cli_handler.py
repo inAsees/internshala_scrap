206068629a1daac8de44c717bda90683b4aa7429
inAsees <gurasees.singh121@gmail.com>
2022-05-03 18:18:56 +0530

    Logger created

--- src/cli_handler.py
+++ src/cli_handler.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from typing import Optional
 from src.scrapper import ScrapInternshala, AttemptsHandler
 from src.utils import Utils
+import logging
+from datetime import datetime
 
 
 class CliHandler:
@@ -29,9 +31,10 @@ class CliHandler:
             sys.exit()
 
         keyword_search = available_keywords[int(user_input)]
+        logging.info(datetime, "Scrapping started")
         scrapper = ScrapInternshala(keyword_search)
         scrapper.scrap_all_pages()
-        print("Scrapping successfully completed.\n")
+        logging.info(datetime, "Scrapping finished at")
         print("Please provide a valid file path to save the data.\n"
               "If you have already saved data and trying to save new data it is highly recommended to provide different"
               " file name.\n")
