d7da463f0d1d65d82e55af5c1a744963c4238e0b
inAsees <gurasees.singh121@gmail.com>
2022-05-03 18:18:10 +0530

    Logging configured

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from src.cli_handler import CliHandler
+import logging
+from pathlib import Path
+
+logging.basicConfig(filename=Path('scrap_logger.log'), encoding='utf-8',
+                    level=logging.INFO)
 
 
 def main():
