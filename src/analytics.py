5a40fa3f6ed4745d424fe35bedec46d829875aec
inAsees <gurasees.singh121@gmail.com>
2022-05-06 00:53:05 +0530

    New method search_top_skills_acc_to_highest_stipend created

--- src/analytics.py
+++ src/analytics.py
@@ -28,7 +28,15 @@ class Analytics:
         self._print_top_results(self._dic, sorted_dic)
 
     def search_top_skills_acc_to_highest_stipend(self) -> None:
-        pass
+        sorted_df = self._df.sort_values("monthly_lump_sum", ascending=False)
+        for i in sorted_df["skill_set"].values:
+            for j in literal_eval(i):
+                if j == "":
+                    continue
+                self._dic[j] = self._dic.get(j, 0) + 1
+        sorted_dic = sorted(self._dic, key=self._dic.get, reverse=True)
+        print("\n* Top 10 skills according to highest stipend are listed below\n")
+        self._print_top_results(self._dic, sorted_dic)
 
     def _print_top_results(self, dic: Dict, sorted_dic: List) -> None:
         counter = 0
@@ -44,3 +52,4 @@ if __name__ == "__main__":
     analytics = Analytics()
     analytics.search_top_skills()
     analytics.search_top_locations()
+    analytics.search_top_skills_acc_to_highest_stipend()
